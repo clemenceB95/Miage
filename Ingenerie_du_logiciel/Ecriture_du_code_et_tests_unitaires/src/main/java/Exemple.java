accbf5469bf31a4b62e9997d3cf185c83da78fc2
Clemence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-10-15 22:56:35 +0200

    fix: correction des erreurs de compilation pour obtenir BUILD SUCCESS

--- Ingenerie_du_logiciel/Ecriture_du_code_et_tests_unitaires/src/main/java/Exemple.java
+++ Ingenerie_du_logiciel/Ecriture_du_code_et_tests_unitaires/src/main/java/Exemple.java
@@ -2,41 +2,24 @@
  * Ma class d'exemple.
  */
 public class Exemple {
-    /** Chaîne de texte associée à l’objet. */
     private final String t;
-    /** Valeur entière associée à l’objet. */
     private int v;
-
-    /**
-     * Constructeur de la classe Exemple.
-     * @param t chaîne de caractères associée à cet objet
-     */
     public Exemple(String t) {
         this.t = t;
     }
-
     /**
-     * Retourne la valeur actuelle de v.
      * @return la valeur de v
      */
     public int getV() {
         return this.v;
     }
-
+    public final void setV(int v){
+        this.v =v;}
     /**
-     * Modifie la valeur de v.
-     * @param v nouvelle valeur entière
-     */
-    public final void setV(int v) {
-        this.v = v;
-    }
-
-    /**
-     * Retourne t si v est positif.
-     * @return la chaîne t si v > 0, sinon une chaîne vide
+     * @return t si v est positif
      */
     public final String getT() {
-        if (v > 0) return t;
+        if ( v > 0) return t;
         return "";
     }
 }
